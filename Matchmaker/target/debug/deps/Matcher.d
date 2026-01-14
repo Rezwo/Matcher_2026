2744ab2c29c47303aaaf2a6d88b1d18fdf2a65ad
AKA_Woosh <renarskrustins9@gmail.com>
2026-01-14 17:17:36 +0200

    go shower

--- Matchmaker/target/debug/deps/Matcher.d
+++ Matchmaker/target/debug/deps/Matcher.d
@@ -1,5 +1,8 @@
-D:\Coding_Completed_2025\Matcher_2026\Matchmaker\target\debug\deps\Matcher.d: src\main.rs
+D:\Coding_Completed_2025\Matcher_2026\Matchmaker\target\debug\deps\Matcher.d: src\main.rs src\Types.rs src\Configurations.rs src\Matchmaker.rs
 
-D:\Coding_Completed_2025\Matcher_2026\Matchmaker\target\debug\deps\Matcher.exe: src\main.rs
+D:\Coding_Completed_2025\Matcher_2026\Matchmaker\target\debug\deps\Matcher.exe: src\main.rs src\Types.rs src\Configurations.rs src\Matchmaker.rs
 
 src\main.rs:
+src\Types.rs:
+src\Configurations.rs:
+src\Matchmaker.rs:
---
+++ Matchmaker/target/debug/deps/native_tls-693a788016414a4f.d
@@ -0,0 +1,8 @@
+D:\Coding_Completed_2025\Matcher_2026\Matchmaker\target\debug\deps\native_tls-693a788016414a4f.d: C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\lib.rs C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\imp\schannel.rs
+
+D:\Coding_Completed_2025\Matcher_2026\Matchmaker\target\debug\deps\libnative_tls-693a788016414a4f.rlib: C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\lib.rs C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\imp\schannel.rs
+
+D:\Coding_Completed_2025\Matcher_2026\Matchmaker\target\debug\deps\libnative_tls-693a788016414a4f.rmeta: C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\lib.rs C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\imp\schannel.rs
+
+C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\lib.rs:
+C:\Users\renar\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\native-tls-0.2.14\src\imp\schannel.rs:
